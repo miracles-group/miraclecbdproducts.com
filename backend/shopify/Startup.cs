19705fac583e109ccae0ec4f29dc8d208332e36f
Dustin Le <lmduc2309@gmail.com>
2019-10-03 17:45:42 +0700

    test DBcontext.

--- backend/shopify/Startup.cs
+++ backend/shopify/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -31,8 +32,9 @@ namespace shopify
                 .AllowAnyMethod()
                 .AllowAnyHeader();
             }));
+            services.AddDbContext<DbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("ConnectionString")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-            
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
